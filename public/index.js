e894c5b2a965b93c0bdd0f327f05dfc045498d51
Chance Wright <cwright489@gmail.com>
2020-04-28 16:36:22 -0600

    added service worker to index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,12 @@
+//Add Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").then(reg => {
+      console.log("Service worker registered.", reg);
+    });
+  });
+}
+
 let transactions = [];
 let myChart;
 
